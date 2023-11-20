b06f470aeb693b6609628f7e24f7cf57dc2c8e04
magalhaestiago <magalhaes_tiago@outlook.com>
2023-11-20 14:38:48 -0300

    Item g

--- library.py
+++ library.py
@@ -40,11 +40,8 @@ class Grafo:
     
     # c) Função que retorna a vizinhança de um vértice v, ou seja, os vértices adjacentes a v
     def viz(self, vertice):
-        
-                
+     
         linha = self.matrizAdjacencia[vertice - 1]
-        
-        
         
         adjacentes = []
         for i in range(self.qtdVertices):
@@ -55,23 +52,59 @@ class Grafo:
 
         
         pass
+    
+    # d) Função que retorna o grau de v, ou seja o número de arestas que incidem em v
+    def d(self, vertice):
         
+        linha = self.matrizAdjacencia[vertice - 1]
+        cont = 0
+        for i in range(self.qtdVertices):
+            if linha[i] == 1:
+                cont = cont + 1
+                
+        return cont
+     
+    # f) Retorna o menor grau presente no grafo
+       
+    def mind(self):
+        menor = 10000
+        
+        for i in range(self.qtdVertices):
+            aux = self.d(i)
+            if aux < menor:
+                menor = aux
+                
+        return menor
+    
+    # g) Retorna o maior grau presente no grafo
+    
+    def maxd(self):
+        maior = 0
+        
+        for i in range(self.qtdVertices):
+            aux = self.d(i)
+            if aux > maior:
+                maior = aux
+                
+        return maior
     
     
 
 g = Grafo(4)
 g.adicionaAresta(2,1)
 g.adicionaAresta(3,1)
-g.adicionaAresta(4,2)
+g.adicionaAresta(4,1)
+# g.adicionaAresta(4,2)
 
 
 g.mostrarMatriz(g.matrizAdjacencia)
 print("Quantidade de vertices: ",g.n())
 print("Quantidade de arestas: ",g.m())
 #print(g.viz(1))
-print(g.viz(2))
-
-
+print(g.viz(3))
+print(g.d(3))
+print(g.mind())
+print(g.maxd())
 
 
 
